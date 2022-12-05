5528778c2341c82102a8c9f66116fd0cc7678672
fanwb <fanwb@stu.pku.edu.cn>
2022-12-05 20:41:33 +0800

    :sparkles:Finish stream in catfs, need to be tested

---
+++ catfs/mio/stream.go
@@ -0,0 +1,152 @@
+package mio
+
+import (
+	"floo/catfs/mio/compress"
+	"floo/catfs/mio/encrypt"
+	"floo/util"
+	log "github.com/sirupsen/logrus"
+	"io"
+)
+
+// Stream is a stream coming from the backend.
+type Stream interface {
+	io.Reader
+	io.Seeker
+	io.Closer
+	io.WriterTo
+}
+
+// NewOutStream creates an OutStream piping data from floo to the outside.
+// `key` is used to decrypt the data. The compression algorithm is read
+// from the stream header.
+func NewOutStream(r io.ReadSeeker, key []byte) (Stream, error) {
+	rEnc, err := encrypt.NewReader(r, key)
+	if err != nil {
+		return nil, err
+	}
+
+	rZip := compress.NewReader(rEnc)
+	return struct {
+		io.Reader
+		io.Seeker
+		io.Closer
+		io.WriterTo
+	}{
+		Reader:   rZip,
+		Seeker:   rZip,
+		WriterTo: rZip,
+		Closer:   io.NopCloser(rZip),
+	}, nil
+}
+
+// NewInStream creates a new stream that pipes data into ipfs.
+// The data is read from `r`, encrypted with `key` and compressed with `algo`.
+func NewInStream(r io.Reader, key []byte, algo compress.AlgorithmType) (io.Reader, error) {
+	pr, pw := io.Pipe()
+
+	// Set up the writer part:
+	wEnc, encErr := encrypt.NewWriter(pw, key)
+	if encErr != nil {
+		return nil, encErr
+	}
+
+	wZip, zipErr := compress.NewWriter(wEnc, algo)
+	if zipErr != nil {
+		return nil, zipErr
+	}
+
+	// Suck the reader empty and move it to `wZip`.
+	// Every write to wZip will be available as read in `pr`.
+	go func() {
+		if _, err := io.Copy(wZip, r); err != nil {
+			// Continue closing the fds; no return.
+			log.Warningf("internal write error: %v", err)
+		}
+
+		if err := wZip.Close(); err != nil {
+			// Continue closing the others:
+			log.Warningf("internal close zip layer error: %v", err)
+		}
+
+		if err := wEnc.Close(); err != nil {
+			// Continue closing the others:
+			log.Warningf("internal close enc layer error: %v", err)
+		}
+
+		if err := pw.Close(); err != nil {
+			log.Warningf("internal close pipe error: %v", err)
+		}
+	}()
+	return pr, nil
+}
+
+// limitedStream is a small wrapper around stream,
+// which allows truncating the stream at certain size.
+// It provides the same
+type limitedStream struct {
+	stream Stream
+	pos    uint64
+	size   uint64
+}
+
+func (ls *limitedStream) Read(buf []byte) (int, error) {
+	isEOF := false
+	if ls.pos+uint64(len(buf)) >= ls.size {
+		buf = buf[:ls.size-ls.pos]
+		isEOF = true
+	}
+
+	n, err := ls.stream.Read(buf)
+	if err != nil {
+		return n, err
+	}
+
+	if isEOF {
+		err = io.EOF
+	}
+	return n, err
+}
+
+func (ls *limitedStream) Seek(offset int64, whence int) (int64, error) {
+	switch whence {
+	case io.SeekCurrent:
+		return ls.Seek(int64(ls.pos)+offset, io.SeekStart)
+	case io.SeekEnd:
+		ls.pos = 0
+		return ls.Seek(int64(ls.size)+offset, io.SeekStart)
+	case io.SeekStart:
+		ls.pos = 0
+	}
+
+	newPos := int64(ls.pos) + offset
+	if newPos < 0 {
+		return -1, io.EOF
+	}
+
+	if newPos > int64(ls.size) {
+		return int64(ls.size), io.EOF
+	}
+
+	ls.pos = uint64(newPos)
+	return ls.stream.Seek(newPos, io.SeekStart)
+}
+
+func (ls *limitedStream) WriteTo(w io.Writer) (int64, error) {
+	// We do not want to defeat the purpose of WriteTo here.
+	// That's why we do the limit check in the writer part.
+	return ls.stream.WriteTo(util.LimitWriter(w, int64(ls.size-ls.pos)))
+}
+
+func (ls *limitedStream) Close() error {
+	return ls.stream.Close()
+}
+
+// LimitStream is like io.LimitReader, but works for mio.Stream.
+// It will not allow reading/seeking after the specified size.
+func LimitStream(stream Stream, size uint64) Stream {
+	return &limitedStream{
+		stream: stream,
+		pos:    0,
+		size:   size,
+	}
+}
--- go.mod
+++ go.mod
@@ -5,49 +5,35 @@ go 1.19
 require (
 	bazil.org/fuse v0.0.0-20200524192727-fb710f7dfd05
 	capnproto.org/go/capnp/v3 v3.0.0-alpha.5
+	github.com/bkaradzic/go-lz4 v1.0.0
+	github.com/dgraph-io/badger/v3 v3.2103.4
+	github.com/golang/snappy v0.0.3
 	github.com/pkg/errors v0.9.1
 	github.com/sahib/config v0.2.0
+	github.com/sdemontfort/go-mimemagic v0.0.0-20150708072242-d026a5785116
 	github.com/sirupsen/logrus v1.9.0
 	github.com/urfave/cli v1.22.10
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	zombiezen.com/go/capnproto2 v2.18.2+incompatible
 )
 
 require (
-	github.com/AndreasBriese/bbloom v0.0.0-20180913140656-343706a395b7 // indirect
-	github.com/bkaradzic/go-lz4 v1.0.0 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dgraph-io/badger v1.5.4 // indirect
-	github.com/dgraph-io/badger/v3 v3.2103.4 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
-	github.com/dgryski/go-farm v0.0.0-20190423205320-6a90982ecee2 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
-	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/flatbuffers v1.12.1 // indirect
-	github.com/gxed/hashland/keccakpg v0.0.1 // indirect
-	github.com/gxed/hashland/murmur3 v0.0.1 // indirect
-	github.com/ipfs/go-ipfs-util v0.0.1 // indirect
 	github.com/klauspost/compress v1.12.3 // indirect
-	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
-	github.com/minio/sha256-simd v0.0.0-20190131020904-2d45a736cd16 // indirect
-	github.com/mr-tron/base58 v1.1.0 // indirect
-	github.com/multiformats/go-multihash v0.0.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/sdemontfort/go-mimemagic v0.0.0-20150708072242-d026a5785116 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	go.opencensus.io v0.22.5 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
 	golang.org/x/sync v0.0.0-20220907140024-f12130a52804 // indirect
 	golang.org/x/sys v0.0.0-20221010170243-090e33056c14 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
