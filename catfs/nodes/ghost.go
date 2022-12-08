73103ce84a35c0ed66e005fe68d477bb0d1ec750
fanwb <fanwb@stu.pku.edu.cn>
2022-12-08 16:36:14 +0800

    :construction:Finish file, directory, ghost and linker

--- catfs/nodes/ghost.go
+++ catfs/nodes/ghost.go
@@ -1,7 +1,9 @@
 package nodes
 
 import (
+	"capnproto.org/go/capnp/v3"
 	ie "floo/catfs/errors"
+	capnp_model "floo/catfs/nodes/capnp"
 	h "floo/util/hashlib"
 	"fmt"
 )
@@ -16,6 +18,23 @@ type Ghost struct {
 	ghostPath  string
 	ghostInode uint64
 	oldType    NodeType
+}
+
+// MakeGhost takes an existing node and converts it to a ghost.
+// In the ghost form no metadata is lost, but the node should
+// not show up. `inode` will be the new inode of the ghost.
+// It should differ to the previous node.
+func MakeGhost(nd ModNode, inode uint64) (*Ghost, error) {
+	if nd.Type() == NodeTypeGhost {
+		panic("cannot put a ghost in a ghost")
+	}
+
+	return &Ghost{
+		ModNode:    nd.Copy(nd.Inode()),
+		oldType:    nd.Type(),
+		ghostInode: inode,
+		ghostPath:  nd.Path(),
+	}, nil
 }
 
 // Type always returns NodeTypeGhost
@@ -72,4 +91,145 @@ func (g *Ghost) Inode() uint64 {
 // SetGhostPath sets the path of the ghost.
 func (g *Ghost) SetGhostPath(newPath string) {
 	g.ghostPath = newPath
+}
+
+// ToCapnp serializes the underlying node
+func (g *Ghost) ToCapnp() (*capnp.Message, error) {
+	msg, seg, err := capnp.NewMessage(capnp.SingleSegment(nil))
+	if err != nil {
+		return nil, err
+	}
+
+	capNd, err := capnp_model.NewRootNode(seg)
+	if err != nil {
+		return nil, err
+	}
+
+	return msg, g.ToCapnpNode(seg, capNd)
+}
+
+// ToCapnpNode converts this node to a serializable capnp proto node.
+func (g *Ghost) ToCapnpNode(seg *capnp.Segment, capNd capnp_model.Node) error {
+	var base *Base
+	capGhost, err := capNd.NewGhost()
+	if err != nil {
+		return err
+	}
+
+	capGhost.SetGhostInode(g.ghostInode)
+	if err = capGhost.SetGhostPath(g.ghostPath); err != nil {
+		return err
+	}
+
+	switch g.oldType {
+	case NodeTypeFile:
+		file, ok := g.ModNode.(*File)
+		if !ok {
+			return ie.ErrBadNode
+		}
+
+		capFile, err := file.setFileAttrs(seg)
+		if err != nil {
+			return err
+		}
+
+		base = &file.Base
+		if err = capGhost.SetFile(*capFile); err != nil {
+			return err
+		}
+	case NodeTypeDirectory:
+		dir, ok := g.ModNode.(*Directory)
+		if !ok {
+			return ie.ErrBadNode
+		}
+
+		capDir, err := dir.setDirectoryAttrs(seg)
+		if err != nil {
+			return err
+		}
+
+		base = &dir.Base
+		if err = capGhost.SetDirectory(*capDir); err != nil {
+			return err
+		}
+	case NodeTypeGhost:
+		panic("Recursive ghosts are not possible")
+	default:
+		panic(fmt.Sprintf("Unknown node type: %d", g.oldType))
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if err := base.setBaseAttrsToNode(capNd); err != nil {
+		return err
+	}
+
+	return capNd.SetGhost(capGhost)
+}
+
+// FromCapnp reads all attributes from a previously marshaled ghost.
+func (g *Ghost) FromCapnp(msg *capnp.Message) error {
+	capNd, err := capnp_model.ReadRootNode(msg)
+	if err != nil {
+		return err
+	}
+
+	return g.FromCapnpNode(capNd)
+}
+
+// FromCapnpNode converts a serialized node to a normal node.
+func (g *Ghost) FromCapnpNode(capNd capnp_model.Node) error {
+	if typ := capNd.Which(); typ != capnp_model.Node_Which_ghost {
+		return fmt.Errorf("bug: ghost unmarshal with non ghost type: %d", typ)
+	}
+
+	capGhost, err := capNd.Ghost()
+	if err != nil {
+		return err
+	}
+
+	g.ghostInode = capGhost.GhostInode()
+	g.ghostPath, err = capGhost.GhostPath()
+	if err != nil {
+		return err
+	}
+
+	var base *Base
+
+	switch typ := capGhost.Which(); typ {
+	case capnp_model.Ghost_Which_directory:
+		capDir, err := capGhost.Directory()
+		if err != nil {
+			return err
+		}
+
+		dir := &Directory{}
+		if err := dir.readDirectoryAttr(capDir); err != nil {
+			return err
+		}
+
+		g.ModNode = dir
+		g.oldType = NodeTypeDirectory
+		base = &dir.Base
+	case capnp_model.Ghost_Which_file:
+		capFile, err := capGhost.File()
+		if err != nil {
+			return err
+		}
+
+		file := &File{}
+		if err := file.readFileAttrs(capFile); err != nil {
+			return err
+		}
+
+		g.ModNode = file
+		g.oldType = NodeTypeFile
+		base = &file.Base
+	default:
+		return ie.ErrBadNode
+	}
+
+	return base.parseBaseAttrsFromNode(capNd)
 }
